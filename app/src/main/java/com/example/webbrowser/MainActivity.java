ea2ac104a19ed12379e4033144f793b118a6c21a
Divakar Choudhary <divakarkumarchy2001@gmail.com>
2023-08-06 12:26:49 +0530

    Initial commit

---
+++ app/src/main/java/com/example/webbrowser/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.webbrowser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+    WebView Brow;
+    ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        progressBar = findViewById(R.id.progressBar);
+
+
+        Brow = findViewById(R.id.brow);
+        Brow.loadUrl("https://chat.openai.com/chat");
+        Brow.setWebViewClient(new WebViewClient());
+        Brow.getSettings().setJavaScriptEnabled(true);
+        getSupportActionBar().hide();
+
+
+    }
+
+    public class WebViewClient extends android.webkit.WebViewClient {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+        }
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            progressBar.setVisibility(View.GONE);
+        }
+    }
+}
